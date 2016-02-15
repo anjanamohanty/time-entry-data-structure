874d0766b2bba2c04bca0a4127dc6974e9aea882
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-15 18:31:09 -0500

    Add migration for task_comments

---
+++ task_comment_migration.rb
@@ -0,0 +1,12 @@
+require 'activerecord'
+
+class TaskCommentMigration < ActiveRecord::Migration
+  def change
+    create_table :task_comments do |t|
+      t.integer :task_id
+      t.integer :developer_id
+      t.datetime :date
+      t.string :comment
+    end
+  end
+end
